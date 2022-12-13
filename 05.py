7eac10542b8a9fb7244945df765f1378cb33e40b
Toben Archer <sandslash@gmail.com>
2022-12-12 19:40:18 -0600

    q5

---
+++ 05.py
@@ -0,0 +1,26 @@
+lines = open('05.txt').read().split('\n')
+
+# Create the stacks:
+line = lines.pop(0)
+stacks = [[] for i in range(int((len(line)+1)/4))]
+while '[' in line:
+    for i,stack in enumerate(stacks):
+        if line[i*4+1] != ' ':
+            stack.append(line[i*4+1])
+    line = lines.pop(0)
+
+# Remove blank lines
+lines.pop(0)
+lines.pop()
+
+print(stacks)
+for line in lines:
+    _, amount, _, origin, _, dest = line.split(' ')
+    for i in range(int(amount)):
+        stacks[int(dest)-1].insert(0, stacks[int(origin)-1].pop(0))
+    print(stacks)
+
+for stack in stacks:
+    print(stack[0], end='')
+
+#Objective: CMZ
